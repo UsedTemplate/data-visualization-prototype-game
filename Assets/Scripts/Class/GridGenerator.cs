b3eee1b7164d5beb089e3fab227129741e7626e9
Tempy <usedtemplate@gmail.com>
2025-09-10 10:04:54 +0200

    Improved GridGenerator, GPU Instancing
    I've redone the calculations for the GridGenerator so that the origin is at the center not one of the corners so it's a bit more easier to control.

--- Assets/Scripts/Class/GridGenerator.cs
+++ Assets/Scripts/Class/GridGenerator.cs
@@ -7,11 +7,14 @@ public class GridGenerator
         Vector3[,] positions = new Vector3[rows, columns];
         Vector3 startPos = origin != null ? origin.position : Vector3.zero;
 
+        float offsetX = (columns - 1) * spacingX * 0.5f;
+        float offsetZ = (rows - 1) * spacingZ * 0.5f;
+
         for (int z = 0; z < rows; z++)
         {
             for (int x = 0; x < columns; x++)
             {
-                positions[z, x] = startPos + new Vector3(x * spacingX, 0, z * spacingZ);
+                positions[z, x] = startPos + new Vector3(x * spacingX - offsetX, 0, z * spacingZ - offsetZ);
             }
         }
 
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,58 +1,85 @@
 using System.Runtime.CompilerServices;
+using TreeEditor;
+using Unity.Collections;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private Mesh mesh;
-    [SerializeField] private Material material;
+    [SerializeField] private Mesh meshTree;
+    [SerializeField] private Material materialTree;
+    [SerializeField] private Mesh meshLeaf;
+    [SerializeField] private Material materialLeaf;
     [SerializeField] private Transform gridParent;
-    private float[] cubeOffsets;
-    private Matrix4x4[] matrices;
+    private float[] treeOffsets;
+    private float[] leafOffsets;
+    private Matrix4x4[] treeMatrices;
+    private Matrix4x4[] leafMatrices;
 
-    private Vector3[] positions;
-    private RenderParams rp;
+    private Vector3[] treePositions;
+    private Vector3[] leafPositions;
+    private RenderParams treeRP;
+    private RenderParams leafRP;
 
     private int xCount = 256;
     private int zCount = 256;
     private float spacing = 1f;
-
 
     void Start()
     {
         Vector3[,] gridPositions = GridGenerator.GenerateGridPositions(xCount, zCount, spacing, spacing, gridParent);
         int count = xCount * zCount;
 
-        positions = new Vector3[count];
-        cubeOffsets = new float[count];
-        matrices = new Matrix4x4[count];
+        treePositions = new Vector3[count];
+        leafPositions = new Vector3[count];
+
+        treeOffsets = new float[count];
+        leafOffsets = new float[count];
+
+        treeMatrices = new Matrix4x4[count];
+        leafMatrices = new Matrix4x4[count];
 
         for (int z = 0; z < gridPositions.GetLength(0); z++)
         {
             for (int x = 0; x < gridPositions.GetLength(1); x++)
             {
-                // GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 int i = z * gridPositions.GetLength(1) + x;
 
-                // cube.transform.position = gridPositions[z, x];
-                // cube.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
-                // cube.name = $"Cube - {i}";
+                treeOffsets[i] = gridPositions[z, x].y;
+                leafOffsets[i] = gridPositions[z, x].y + 0.9f;
 
-                cubeOffsets[i] = gridPositions[z, x].y;
-                positions[i] = gridPositions[z, x];
+                treePositions[i] = gridPositions[z, x];
+                leafPositions[i] = gridPositions[z, x];
             }
         }
 
-        rp = new RenderParams(material);
+        treeRP = new RenderParams(materialTree);
+        leafRP = new RenderParams(materialLeaf);
+    }
+
+    private void RenderTrees()
+    {
+        for (var i = 0; i < treePositions.Length; i++)
+        {
+            treeMatrices[i].SetTRS(treePositions[i], Quaternion.identity, new Vector3(0.2f, 0.2f, 0.2f));
+        }
+
+        Graphics.RenderMeshInstanced(treeRP, meshTree, 0, treeMatrices);
+    }
+
+    private void RenderLeaves()
+    {
+        for (var i = 0; i < leafPositions.Length; i++)
+        {
+            leafMatrices[i].SetTRS(leafPositions[i] + new Vector3(0, leafOffsets[i], 0), Quaternion.identity, new Vector3(0.2f, 0.2f, 0.2f));
+        }
+
+        Graphics.RenderMeshInstanced(leafRP, meshLeaf, 0, leafMatrices);
     }
 
     void Update()
     {
-        for (var i = 0; i < positions.Length; i++)
-        {
-            matrices[i].SetTRS(positions[i], Quaternion.identity, Vector3.one);
-        }
-
-        Graphics.RenderMeshInstanced(rp, mesh, 0, matrices);
+        RenderTrees();
+        RenderLeaves();
     }
 }
